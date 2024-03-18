8fc42c17f5167e5d68b0fb5476b63356df913ec8
Korolenko Daria <admem75213@gmail.com>
2024-03-18 17:24:21 +0200

    added correct data and merged with main

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,26 @@
-import dotenv from 'dotenv';
-import HttpError from '../helpers/HttpError';
-import jwt from 'jsonwebtoken';
-import User from '../db/models/User';
+import dotenv from "dotenv";
+import HttpError from "../helpers/HttpError.js";
+import jwt from "jsonwebtoken";
+import User from "../db/models/User.js";
 dotenv.config();
 
 const { SECRET_KEY } = process.env;
 
 export const authenticate = async (req, res, next) => {
-  const { authorization = '' } = req.headers;
+  const { authorization = "" } = req.headers;
 
-  const [bearer, token] = authorization.split(' ');
+  const [bearer, token] = authorization.split(" ");
 
   try {
-    if (bearer !== 'Bearer') {
-      throw HttpError(401, 'Not Authorized');
+    if (bearer !== "Bearer") {
+      throw HttpError(401, "Not Authorized");
     }
 
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
     if (!user || !user.token || user.token !== token) {
-      throw HttpError(401, 'Not Authorized');
+      throw HttpError(401, "Not Authorized");
     }
 
     req.user = user;
